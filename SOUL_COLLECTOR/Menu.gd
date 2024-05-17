c2fb2a8bcc0c8ee6f272d478afc7d65d6e447122
x40Arcade <139607164+KennesuBakkuro@users.noreply.github.com>
2024-05-17 21:48:29 +0800

    Add files via upload

---
+++ SOUL_COLLECTOR/Menu.gd
@@ -0,0 +1,11 @@
+extends Control
+
+#this is when the button play gets pressed
+func _on_play_pressed():
+	get_tree().change_scene_to_file("res://testgame.tscn")
+	#this is for switching the tscn you can just drag the tscn to chage it
+
+
+#this is when the button play gets pressed
+func _on_options_pressed():
+	pass # Replace with function body.
