9f4692c46e736c7a591d0d6b8a075bfe2b79cfeb
x40Arcade <139607164+KennesuBakkuro@users.noreply.github.com>
2024-05-19 09:33:45 +0800

    Delete SOUL_COLLECTOR directory

--- SOUL_COLLECTOR/red-particle.gd
+++
@@ -1,2 +0,0 @@
-extends AnimatedSprite2D
-
--- SOUL_COLLECTOR/testgame.gd
+++
@@ -1,8 +0,0 @@
-extends Node2D
-
-
-
-
-func _on_button_pressed():
-	get_tree().change_scene_to_file("res://menu.tscn")
-	#this is for switching the tscn you can just drag the tscn to chage it
