da32cf9e843a83a387e39e76b657360bb1327263
x40Arcade <139607164+KennesuBakkuro@users.noreply.github.com>
2024-05-20 09:46:16 +0800

    fixes

--- cutscene.gd
+++ cutscene.gd
@@ -4,8 +4,8 @@ extends Control
 
 
 func _on_video_stream_player_finished():
-	get_tree().change_scene_to_file("res://Scene/testgame.tscn")
+	get_tree().change_scene_to_file("res://Scene/map_a_1.tscn")
 
 
 func _on_button_pressed():
-	get_tree().change_scene_to_file("res://Scene/testgame.tscn")
+	get_tree().change_scene_to_file("res://Scene/map_a_1.tscn")
--- loading.gd
+++ loading.gd
@@ -3,4 +3,4 @@ extends Node
 #ung 3.0 ung timer bago lumipat sa scene
 func _ready():
 	await get_tree().create_timer(2.0).timeout
-	get_tree().change_scene_to_file("res://chapter.tscn")
+	get_tree().change_scene_to_file("res://Scene/chapter.tscn")
